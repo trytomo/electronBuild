ade2a01d928fc5cc598cf436611083e220dc07f2
aris <arisrizkafauzi@gmail.com>
2021-10-07 02:02:51 +0700

    Create Repository entity

---
+++ app/src/main/java/com/d2i/stockmanagement/entity/Diamond.java
@@ -0,0 +1,93 @@
+package com.d2i.stockmanagement.entity;
+
+public class Diamond {
+    public String rfid;
+
+    public String getRfid() {
+        return rfid;
+    }
+
+    public void setRfid(String rfid) {
+        this.rfid = rfid;
+    }
+
+    public String sku;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String nomorDesain;
+
+    public String getNomorDesain() {
+        return nomorDesain;
+    }
+
+    public void setNomorDesain(String nomorDesain) {
+        this.nomorDesain = nomorDesain;
+    }
+
+    public String berat;
+
+    public String getBerat() {
+        return berat;
+    }
+
+    public void setBerat(String berat) {
+        this.berat = berat;
+    }
+
+    public String kadar;
+
+    public String getKadar() {
+        return kadar;
+    }
+
+    public void setKadar(String kadar) {
+        this.kadar = kadar;
+    }
+
+    public String harga;
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String beratBerlian1;
+
+    public String getBeratBerlian1() {
+        return beratBerlian1;
+    }
+
+    public void setBeratBerlian1(String beratBerlian1) {
+        this.beratBerlian1 = beratBerlian1;
+    }
+
+    public String jumlahBerlian1;
+
+    public String getJumlahBerlian1() {
+        return jumlahBerlian1;
+    }
+
+    public void setJumlahBerlian1(String jumlahBerlian1) {
+        this.jumlahBerlian1 = jumlahBerlian1;
+    }
+
+    public String beratBerlian2;
+
+    public String getBeratBerlian2() {
+        return beratBerlian2;
+    }
+
+    public void setBeratBerlian2(String beratBerlian2) {
+        this.beratBerlian2 = beratBerlian2;
+    }
+}
---
+++ app/src/main/java/com/d2i/stockmanagement/screen/repair/RepairRepository.java
@@ -0,0 +1,4 @@
+package com.d2i.stockmanagement.screen.repair;
+
+public class RepairRepository {
+}
